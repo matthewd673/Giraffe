b0207dd4cd1f8af85b8a7c8df9ff5ace9067ba37
Matthew Daly <matthewd673@outlook.com>
2024-12-30 16:19:08 -0800

    Fix HasEpsilon bug, remove Epsilon from sets

--- Giraffe/src/Grammar.cs
+++ Giraffe/src/Grammar.cs
@@ -5,13 +5,12 @@ namespace Giraffe;
 
 public class Grammar(Dictionary<string, Regex> terminalDefs,
                      List<Production> productions) {
-  public const string Eof = "Eof";
-  public const string Epsilon = "{}";
+  public const string Eof = "$$";
 
-  private Dictionary<string, Regex> terminalDefs = terminalDefs;
   public ImmutableList<Production> Productions { get; } =
     ImmutableList.CreateRange(productions);
 
+  private readonly HashSet<string> hasEpsilon = [];
   private readonly Dictionary<string, HashSet<string>> first = [];
   private readonly Dictionary<string, HashSet<string>> follow = [];
   private readonly Dictionary<int, HashSet<string>> predict = [];
@@ -30,12 +29,14 @@ public class Grammar(Dictionary<string, Regex> terminalDefs,
   /// marks nonterminals that have epsilon productions.
   /// </summary>
   public void ComputeSets() {
-    HashSet<string> nonterminals = productions.Select(p => p.Name).ToHashSet();
+    foreach (Production epsilonProduction in productions.Where(p => p.Count == 0)) {
+      hasEpsilon.Add(epsilonProduction.Name);
+    }
 
-    foreach (string nonterminal in nonterminals) {
+    foreach (string nonterminal in Nonterminals) {
       first.Add(nonterminal, ComputeFirst(nonterminal));
     }
-    foreach (string nonterminal in nonterminals) {
+    foreach (string nonterminal in Nonterminals) {
       follow.Add(nonterminal, ComputeFollow(nonterminal));
     }
     for (int i = 0; i < productions.Count; i++) {
@@ -54,98 +55,89 @@ public class Grammar(Dictionary<string, Regex> terminalDefs,
   }
 
   /// <summary>
-  /// Check if a nonterminal has an epsilon production. Requires sets for this
-  /// grammar to be computed.
+  /// Check if a nonterminal has an epsilon production. Requires that sets for this
+  /// grammar have been computed.
   /// </summary>
   /// <param name="nonterminal">The nonterminal to check.</param>
   /// <returns>
   ///   <c>true</c> if the nonterminal has an epsilon production,
   ///   <c>false</c> otherwise.
   /// </returns>
-  public bool HasEpsilon(string nonterminal) =>
-    ComputeFirst(nonterminal).Contains(Epsilon);
+  public bool HasEpsilon(string nonterminal) => hasEpsilon.Contains(nonterminal);
 
   /// <summary>
-  /// Enumerate the FIRST set of a nonterminal. Requires sets for this grammar
-  /// to be computed.
+  /// Enumerate the FIRST set of a nonterminal. Requires that sets for this grammar
+  /// have been computed.
   /// </summary>
   /// <param name="nonterminal">The nonterminal to enumerate the FIRST set for.</param>
   /// <returns>An IEnumerable of the terminals in the nonterminal's FIRST set.</returns>
-  public IEnumerable<string> First(string nonterminal) =>
-    first[nonterminal].Where(t => !t.Equals(Epsilon));
+  public IEnumerable<string> First(string nonterminal) => first[nonterminal];
 
   /// <summary>
-  /// Enumerate the FOLLOW set of a nonterminal. Requires sets for this grammar
-  /// to be computed.
+  /// Enumerate the FOLLOW set of a nonterminal. Requires that sets for this grammar
+  /// have been computed.
   /// </summary>
   /// <param name="nonterminal">The nonterminal to enumerate the FOLLOW set for.</param>
   /// <returns>An IEnumerable of the terminals in the nonterminal's FOLLOW set.</returns>
-  public IEnumerable<string> Follow(string nonterminal) =>
-    follow[nonterminal].Where(t => !t.Equals(Epsilon));
+  public IEnumerable<string> Follow(string nonterminal) => follow[nonterminal];
 
   /// <summary>
-  /// Enumerate the PREDICT set of a production. Requires sets for this grammar
-  /// to be computed.
+  /// Enumerate the PREDICT set of a production. Requires that sets for this grammar
+  /// have been computed.
   /// </summary>
   /// <param name="index">
   ///   The index in the productions list of the production to enumerate the
   ///   PREDICT set for.
   /// </param>
   /// <returns>An IEnumerable of the terminals in the production's PREDICT set.</returns>
-  public IEnumerable<string> Predict(int index) =>
-    predict[index].Where(t => !t.Equals(Epsilon));
+  public IEnumerable<string> Predict(int index) => predict[index];
 
   private HashSet<string> ComputeFirst(string nonterminal) {
     // Re-use value if this has been computed in a previous step.
-    if (this.first.TryGetValue(nonterminal, out HashSet<string>? existingFirst)) {
+    if (first.TryGetValue(nonterminal, out HashSet<string>? existingFirst)) {
       return existingFirst;
     }
 
-    HashSet<string> first = [];
+    HashSet<string> firstSet = [];
 
     // Check every production of the nonterminal (except for epsilon).
     foreach (Production production in
              productions.Where(p => p.Name.Equals(nonterminal))) {
-      first.UnionWith(ComputeFirst(production));
+      firstSet.UnionWith(ComputeProductionFirst(production));
     }
 
-    return first;
+    return firstSet;
   }
 
-  private HashSet<string> ComputeFirst(Production production) {
-    // If this is an epsilon production, epsilon is added to FIRST
-    if (production.Count == 0) {
-      return [Epsilon];
-    }
-
-    HashSet<string> first = [];
+  private HashSet<string> ComputeProductionFirst(Production production) {
+    HashSet<string> firstSet = [];
 
     foreach (string term in production) {
       // If the first item is a terminal then it is in FIRST and we're done
       if (IsTerminal(term)) {
-        first.Add(term);
+        firstSet.Add(term);
         break;
       }
 
       // If the first item is a nonterminal, then its entire FIRST set is in FIRST
       // Additionally, if it has an epsilon production then we'll need to add the
       // next item in the sequence to FIRST as well.
-      first.UnionWith(ComputeFirst(term));
+      firstSet.UnionWith(ComputeFirst(term));
       if (!HasEpsilon(term)) {
         break;
       }
     }
 
-    return first;
+    return firstSet;
   }
 
   private HashSet<string> ComputeFollow(string nonterminal) {
     // Re-use value if this has been computed in a previous step.
-    if (this.follow.TryGetValue(nonterminal, out HashSet<string>? existingFollow)) {
+    if (follow.TryGetValue(nonterminal, out HashSet<string>? existingFollow)) {
       return existingFollow;
     }
 
-    HashSet<string> follow = [];
+    HashSet<string> followSet = [];
 
     foreach (Production production in productions) {
       // For every occurrence of the given nonterminal in the production...
@@ -154,14 +146,14 @@ public class Grammar(Dictionary<string, Regex> terminalDefs,
           // If we've reached the end of the production, then add the
           // productions' FOLLOW set to FOLLOW
           if (i == production.Count) {
-            follow.UnionWith(ComputeFollow(production.Name));
+            followSet.UnionWith(ComputeFollow(production.Name));
             break;
           }
 
           string next = production[i];
           // If it is followed by a terminal, then add that terminal to FOLLOW
           // If it is followed by a nonterminal, then add that nonterminal's FIRST set to FOLLOW
-          follow.UnionWith(IsTerminal(next) ? [next] : First(next));
+          followSet.UnionWith(IsTerminal(next) ? [next] : First(next));
 
           // If it is followed by a nonterminal with an epsilon production, keep looking
           if (!HasEpsilon(next)) {
@@ -171,15 +163,24 @@ public class Grammar(Dictionary<string, Regex> terminalDefs,
       }
     }
 
-    return follow;
+    return followSet;
   }
 
   private HashSet<string> ComputePredict(Production production) {
-    HashSet<string> predict = ComputeFirst(production);
-    if (predict.Contains(Epsilon)) {
-      predict.UnionWith(Follow(production.Name));
+    HashSet<string> predictSet = [];
+
+    HashSet<string> productionFirst = ComputeProductionFirst(production);
+    predictSet.UnionWith(productionFirst);
+
+    // If this production is an epsilon production, or if every item in the production can be epsilon,
+    // then we should also add the FOLLOW set to the PREDICT set.
+    int nonEpsCt = production.Count(e => IsTerminal(e) || !HasEpsilon(e));
+
+    if (nonEpsCt == 0) {
+      predictSet.UnionWith(Follow(production.Name));
     }
-    return predict;
+
+    return predictSet;
   }
 
   private static IEnumerable<int> IndexOfAll<T>(IEnumerable<T> sequence, T element) =>
